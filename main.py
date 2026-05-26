d119fb901c5b9d88a5496931c3581b6978d998e2
Xaphan64 <alex_daniel64@yahoo.com>
2026-05-26 20:51:55 +0300

    added api key

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from agent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if api_key == None:
+    raise RuntimeError("API key is missing")
+client = genai.Client(api_key=api_key)
+
+def main():
+    response = client.models.generate_content(model = "gemini-2.5-flash" , contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    print(response.text)
 
 if __name__ == "__main__":
     main()
