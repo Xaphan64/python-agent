471b1d84beaaac7fcdb6e97d65bdca0cc245e3ae
Xaphan64 <alex_daniel64@yahoo.com>
2026-06-01 20:58:09 +0300

    added argparse

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+
+parser = argparse.ArgumentParser(description="Agent")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,7 +15,7 @@ if api_key == None:
 client = genai.Client(api_key=api_key)
 
 def main():
-    response = client.models.generate_content(model = "gemini-2.5-flash" , contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    response = client.models.generate_content(model = "gemini-2.5-flash" , contents=args.user_prompt)
     if response.usage_metadata == None:
         raise RuntimeError('Metadata is missing')
     else:
