f3698d484990b2d93f4be2fc9c72c290e1899308
Xaphan64 <alex_daniel64@yahoo.com>
2026-06-02 08:39:11 +0300

    added verbose and multiple messages

--- main.py
+++ main.py
@@ -2,26 +2,36 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
-
-parser = argparse.ArgumentParser(description="Agent")
-parser.add_argument("user_prompt", type=str, help="User prompt")
-args = parser.parse_args()
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-if api_key == None:
-    raise RuntimeError("API key is missing")
-client = genai.Client(api_key=api_key)
+from google.genai import types
 
 def main():
-    response = client.models.generate_content(model = "gemini-2.5-flash" , contents=args.user_prompt)
+    parser = argparse.ArgumentParser(description="Agent")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    if args.verbose:
+        print(f'User prompt: {args.user_prompt}')
+
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key == None:
+        raise RuntimeError("API key is missing")
+
+    client = genai.Client(api_key=api_key)    
+    messages: list[types.Content] = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    generate_content(client, messages, args.verbose)
+    
+def generate_content(client: genai.Client, messages: list[types.Content], verbose: bool) -> None:
+    response = client.models.generate_content(model = "gemini-2.5-flash", contents=messages)
     if response.usage_metadata == None:
         raise RuntimeError('Metadata is missing')
-    else:
+
+    if verbose:
         print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
         print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
-    print(response.text)
+        print(response.text)
 
 if __name__ == "__main__":
     main()
